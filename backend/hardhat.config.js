dc08f17c48f8f43a1c88d6750eee8a685c10ed58
Badu Blanc <itsbadu@hey.com>
2022-03-04 17:35:31 -0600

    add hardhat-abi-exporter

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-abi-exporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.9",
+  abiExporter: {
+    path: "../site/src/lib/contracts",
+    runOnCompile: true,
+    clear: true,
+    pretty: true,
+    flat: true,
+    only: [":BlancosNFT$"],
+  },
 };
