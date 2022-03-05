2a8c3bcc2816a3a2d5d554a742362f05f1478fdf
Badu Blanc <itsbadu@hey.com>
2022-03-04 18:00:20 -0600

    site scaffolding

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import { ethers } from 'ethers';
+
+	let provider, signer, address, ensName;
+
+	const connectWallet = async () => {
+		await provider.send('eth_requestAccounts', []);
+		signer = provider.getSigner();
+		address = await signer.getAddress();
+		ensName = await checkForENS(address);
+	};
+
+	const checkForENS = async (_address) => {
+		if (!_address) return null;
+		return (await provider.lookupAddress(_address)) || _address;
+	};
+
+	onMount(async () => {
+		if (window.ethereum) {
+			provider = new ethers.providers.Web3Provider(window.ethereum);
+		}
+	});
+</script>
+
+{#if provider}
+	{#if signer}
+		<p>Hi, {ensName || address}!</p>
+	{:else}
+		<button on:click={connectWallet}>Connect Wallet</button>
+	{/if}
+{:else}
+	<p>install metamask ;)</p>
+{/if}
