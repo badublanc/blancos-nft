208ced2620018ee77f38a8c8d9142d8a0239dbd6
Badu Blanc <itsbadu@hey.com>
2022-03-07 15:49:16 -0600

    add mint cost, hyperlink logo

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -36,49 +36,53 @@
 			<p class="font-light text-lg sm:text-2xl mb-1">&#8470; 4</p>
 			<img src="https://i.imgur.com/YknzXNF.png" alt="" class="px-2 sm:px-4" />
 
-			<div class="flex items-stretch my-5 sm:my-8 space-x-2">
-				<a
-					href="#"
-					class="flex items-center px-2 hover:bg-gray-800 hover:text-white transition-all duration-300"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						stroke-width="2"
+			<div class="my-5 sm:my-8 text-center">
+				<div class="flex items-stretch space-x-2">
+					<a
+						href="#"
+						class="flex items-center px-2 hover:bg-gray-800 hover:text-white transition-all duration-300"
 					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-						/>
-					</svg>
-					<p class="">Prev</p>
-				</a>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							stroke-width="2"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+							/>
+						</svg>
+						<p class="">Prev</p>
+					</a>
 
-				<button
-					class="sm:text-lg uppercase border border-gray-800 px-4 py-1 bg-gray-800 text-white hover:bg-purple-200 hover:text-black hover:animate-spin transition-all duration-300"
-					>Mint</button
-				>
-
-				<a
-					href="#"
-					class="flex items-center px-2 hover:bg-gray-800 hover:text-white transition-all duration-300"
-				>
-					<p class="">Next</p>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						stroke-width="2"
+					<button
+						class="sm:text-lg uppercase border border-gray-800 px-4 py-1 bg-gray-800 text-white hover:bg-purple-200 hover:text-black hover:animate-spin transition-all duration-300"
+						>Mint</button
 					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
-					</svg>
-				</a>
+
+					<a
+						href="#"
+						class="flex items-center px-2 hover:bg-gray-800 hover:text-white transition-all duration-300"
+					>
+						<p class="">Next</p>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							stroke-width="2"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+						</svg>
+					</a>
+				</div>
+
+				<p class="mt-3 sm:mt-4 text-sm">0.06 &Xi; per mint</p>
 			</div>
 		</div>
 	</div>
